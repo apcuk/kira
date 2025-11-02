e84be2282faed6a8e49ab85f4ebeca9e1d7e308a
apcuk <apcuk@apcuk.ru>
2025-11-02 22:43:26 +0300

    v.3.00

---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py
+
+# v.3.00 - Heloo, World. настройка скруктуры проекта, github'а, .env, .gitignore, requirements.txt 
+
+def main():
+    print("v 3.00 started. hello, world!")
+
+if __name__ == "__main__":
+    main()
